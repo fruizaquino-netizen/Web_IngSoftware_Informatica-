781db23e062e66d31a6e4c5716e800b1835cf8e6
nashe0209 <113204052+nashe0209@users.noreply.github.com>
2026-02-06 00:23:17 -0600

    Brenda - Aspirantes / Proyectos
    Se agrego testimonios a aspirantes y se mejoro proyectos

--- SitioWebCarrera/src/app/pages/proyectos/proyectos-page.component.ts
+++ SitioWebCarrera/src/app/pages/proyectos/proyectos-page.component.ts
@@ -8,16 +8,33 @@ import { CommonModule } from '@angular/common';
   templateUrl: './proyectos-page.component.html',
   styleUrls: ['./proyectos-page.component.css']
 })
-export class ProyectoPageComponent {
 
+export class ProyectoPageComponent {
   galleryImages = [
+    // Áreas físicas
     'assets/img/Electronica1.jpg',
     'assets/img/Elect.jpg',
     'assets/img/SalaRedes.jpg',
     'assets/img/Auditorio.jpg',
     'assets/img/Estatua.jpg',
-    'assets/img/Electronica2.jpg'
+    'assets/img/Electronica2.jpg',
+    'assets/img/ConcursoAltar1.jpg',
+    'assets/img/ConcursoAltar2.jpg',
+    'assets/img/Viaje_EscNaval1.jpg',
+    'assets/img/Viaje_EscNaval2.jpg',
+    'assets/img/Viaje_EscNaval3.jpg',
+    'assets/img/Viaje_Supercool.jpg',
+    'assets/img/Viaje_Thyssenkrupp1.png',
+    'assets/img/Viaje_Thyssenkrupp2.png',
+    'assets/img/Viaje_Thyssenkrupp3.jpg',
+    'assets/img/Viaje_Thyssenkrupp4.jpg',
+    'assets/img/Viaje_UDLAP1.jpg',
+    'assets/img/Viaje_UDLAP2.jpg',
+    'assets/img/Viaje_UDLAP3.jpg',
+    'assets/img/Viaje_UniVeracruz.jpg',
+    'assets/img/Curso_2023.jpg'
   ];
+
 
   activeImage = 0;
   galleryOpen = false;
@@ -25,10 +42,12 @@ export class ProyectoPageComponent {
   openGallery(index: number) {
     this.activeImage = index;
     this.galleryOpen = true;
+    document.body.style.overflow = 'hidden';
   }
 
   closeGallery() {
     this.galleryOpen = false;
+    document.body.style.overflow = '';
   }
 
   next() {
